6cb9cb06a73e4a2b367f0b3cd9ba064820c08849
cfl2314 <cfl2314@163.com>
2018-08-10 23:15:47 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <router-view/>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import MHeader from './components/m-header/m-header'
+export default {
+  name: 'App',
+  components:{
+    MHeader
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "./common/stylus/variable";
+#app 
+  color:$color-theme
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+import fastclick from 'fastclick'
+
+import './common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // router,
+  render:h=>h(App)
+})
