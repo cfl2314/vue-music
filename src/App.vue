59e2058cd62136fc11e74d407b31f27bf7181c8f
cfl2314 <cfl2314@163.com>
2018-08-12 20:07:55 +0800

    加了一个推荐歌单

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
     <m-header></m-header>
-    <router-view/>
+    <tab></tab>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import MHeader from './components/m-header/m-header'
+import Tab from './components/tab/tab'
 export default {
   name: 'App',
   components:{
-    MHeader
+    MHeader,
+    Tab
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 @import "./common/stylus/variable";
-#app 
+#app
   color:$color-theme
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Recommend from '../components/recommend/recommend.vue'
+import Rank from '../components/rank/rank.vue'
+import Singer from '../components/singer/singer.vue'
+import Search from '../components/search/search.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'/',
+      redirect:'/recommend'
+    },
+    {
+      path: '/recommend',
+      name: 'Recommend',
+      component:Recommend
+    },{
+      path: '/rank',
+      name: 'Rank',
+      component:Rank
+    },{
+      path: '/singer',
+      name: 'Singer',
+      component:Singer
+    },{
+      path: '/search',
+      name: 'Search',
+      component:Search
     }
   ]
 })
