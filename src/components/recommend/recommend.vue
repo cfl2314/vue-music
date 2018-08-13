21de506b2f5c59a532223bc5c8416001e23c53d3
cfl2314 <cfl2314@163.com>
2018-08-13 17:19:23 +0800

    加入歌手列表

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,79 +1,92 @@
 <template>
   <div class="recommend">
     <scroll ref="scroll" class="recommend-content" :data="discList">
-      <div v-if="recommends.length" class="slider-wrapper">
-        <slider>
-          <div v-for="(item,index) in recommends" :key="index">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl"/>
-            </a>
-          </div>
-        </slider>
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper">
+          <slider>
+            <div v-for="(item,index) in recommends" :key="index">
+              <a :href="item.linkUrl">
+                <img class="needsclick" :src="item.picUrl" @load="loadImage"/>
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="(item,index) in discList" class="item" :key="index">
+              <div class="icon">
+                <img v-lazy="item.imgurl" width="60" height="60"/>
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li v-for="(item,index) in discList" class="item" :key="index">
-            <div class="icon">
-              <img :src="item.imgurl" width="60" height="60"/>
-            </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
 </template>
 <script type="text/ecmascript-6">
-import Scroll from 'base/scroll/scroll'
-import Slider from 'base/slider/slider'
-import { getRecommend, getDiscList } from 'api/recommend'
-import { ERR_OK } from 'api/config'
+import Loading from 'base/loading/loading'
+import Scroll from "base/scroll/scroll";
+import Slider from "base/slider/slider";
+import { getRecommend, getDiscList } from "api/recommend";
+import { ERR_OK } from "api/config";
 export default {
-  data () {
+  data() {
     return {
       recommends: [],
       discList: []
-    }
+    };
   },
-  created () {
-    this._getRecommend()
-    this._getDiscList()
+  created() {   
+      this._getRecommend()
+      this._getDiscList()  
   },
   methods: {
-    _getRecommend () {
+    _getRecommend() {
       getRecommend().then(res => {
         if (res.code === ERR_OK) {
           this.recommends = res.data.slider
           // console.log(this.recommends)
         }
-      })
+      });
     },
-    _getDiscList () {
+    _getDiscList() {
       getDiscList().then(res => {
         if (res.code === ERR_OK) {
           this.discList = res.data.list
         }
-      })
+      });
+    },
+    loadImage(){
+      if(!this.checkLoaded){
+        this.$refs.scroll.refresh()
+        this.checkLoaded=true
+      }  
     }
   },
   components: {
     Slider,
-    Scroll
+    Scroll,
+    Loading
   }
-}
+};
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
 @import '../../common/stylus/variable';
 
 .recommend {
-  // position: fixed;
-  // width: 100%;
-  // top: 88px;
-  // bottom: 0;
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
   .recommend-content {
     height: 100%;
     overflow: hidden;
